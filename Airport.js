c71e23b80e8fbf4a926dd0506be1e71637ffcd27
bnalvarado <brian.alvarado@proximitycr.com>
2018-11-14 13:14:29 -0600

    make API calls on Airport file

--- Airport.js
+++ Airport.js
@@ -6,20 +6,58 @@ import {
   Text,
   TouchableHighlight,
   TextInput,
-  StyleSheet
+  StyleSheet,
+  ActivityIndicator,
+  FlatList
 } from 'react-native';
+import GLOBALS from './Globals';
 
 class Airport extends Component {
+  // Constructor
+  constructor(props){
+    super(props);
+    this.state = { isLoading: true }
+  }
 
-  render() {
-    return (
+  // Execute code when view load
+  componentDidMount(){
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.movies,
+        }, function(){
+        });
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+  // Render view
+  render(){
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+    return(
       <View style={styles.fieldContainer}>
-        <Text style={styles.text}>Airport</Text>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => 
+            <Text style={styles.text}>{item.title}, {item.releaseYear}</Text>}
+          keyExtractor={({id}, index) => id}
+        />
       </View>
     );
   }
 }
 
+// Define style for page view
 const styles = StyleSheet.create({
   fieldContainer: {
     flex: 1,
